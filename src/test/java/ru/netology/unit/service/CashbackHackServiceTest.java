8f23dc910e361a525d3558fcd2c55b24e04ff820
Evgeniy <varnavskiyev@gmail.com>
2023-01-08 10:26:39 +0300

    add junit4 and tests

--- src/test/java/ru/netology/unit/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/service/CashbackHackServiceTest.java
@@ -1,22 +1,33 @@
 package ru.netology.unit.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
-import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-        @Test
-        public void getBalance() {
-            CashbackHackService cash = new CashbackHackService();
+    @Test
+    public void getCashback() {
+        CashbackHackService cash = new CashbackHackService();
 
-            int amount = 900;
-            int actual = cash.remain(amount);
-            int expected = 100;
+        int amount = 1000;
+        int actual = cash.remain(amount);
+        int expected = 0;
 
-            Assert.assertEquals(actual, expected);
-        }
-
+        Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void getCashback2() {
+        CashbackHackService cash = new CashbackHackService();
+
+        int amount = 900;
+        int actual = cash.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+}
 
